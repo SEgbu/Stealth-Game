b60fc028ebb3ee23c7238439d7b7eb80731befdd
Sincere <sincerityegbu@gmail.com>
2022-02-17 23:00:28 +0000

    OpenGL Learning: Created a window
    + Created a window
    + Added ability to resize the window
    + Created OpenGL context
    + Adjusted compiler settings

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,65 @@
 #include <glad/glad.h>
 #include <glfw/glfw3.h>
 
+// Window dimensions
+const int SCREEN_WIDTH = 800;
+const int SCREEN_HEIGHT = 600;
+
+// This changes the size of the viewport as the window is resized.
+void framebufferSizeCallback(GLFWwindow* window, int width, int heigth);
+
+// Processes inputs
+void processInput(GLFWwindow* window);
+
 int main(){ 
+    // Initializing GLFW
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    // Creating the window
+    GLFWwindow* window = glfwCreateWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "Stealth Game", NULL, NULL);
+    if (window == NULL){
+        std::cout << "GLFW failed to create window" << std::endl;
+        glfwTerminate();
+    }  
+    glfwMakeContextCurrent(window); // Creates the OpenGL context for window
+
+    // Initializing GLAD
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)){ // Gets address of the function from this context
+        std::cout << "Failed to initialize GLAD" << std::endl;
+        return -1;
+    }
+
+    // Set the size of the rendering viewport
+    glViewport(0, 0, SCREEN_WIDTH, SCREEN_HEIGHT);
+
+    // Set the change in the window size to GLFW
+    glfwSetFramebufferSizeCallback(window, framebufferSizeCallback);
+
+    // Render Loop
+    while(!glfwWindowShouldClose(window)){
+        processInput(window); // Inputs
+
+        // Rendering commands
+        glClearColor(0.0f, 0.4f, 0.4f, 1.0f); // Set color which clears frame
+        glClear(GL_COLOR_BUFFER_BIT); // Set which buffer bit were clearing (the colour one).
+
+        glfwSwapBuffers(window); // Swaps front and back buffers
+        glfwPollEvents(); // Checks if events are triggered
+    }
+
+    glfwTerminate(); // End program
     return 0;
+}
+
+void framebufferSizeCallback(GLFWwindow* window, int width, int height){
+    glViewport(0, 0, width, height);
+}
+
+void processInput(GLFWwindow* window){
+    if (glfwGetKey(window, GLFW_KEY_ESCAPE) || glfwGetKey(window, GLFW_KEY_X)){
+        glfwSetWindowShouldClose(window, true);
+    }
 }
