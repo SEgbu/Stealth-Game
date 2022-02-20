7fbe983aa45d06dd457748dc667b0e519a52e87a
Sincere <sincerityegbu@gmail.com>
2022-02-20 10:14:24 +0000

    OpenGL Learning: Made a triangle
    + Put intialization code outside render loop
    + Add shaders, vbos, vaos

--- src/main.cpp
+++ src/main.cpp
@@ -39,49 +39,107 @@ int main(){
     // Set the change in the window size to GLFW
     glfwSetFramebufferSizeCallback(window, framebufferSizeCallback);
 
+    // Vertex data
+    float vertices[] = {
+        0.0f, 0.5f, 0.0f,
+        -0.5f, -0.5f, 0.0f,
+        0.5f, -0.5f, 0.0f
+    };
+
+    // Creating the vertex array object
+    unsigned int VAO;
+    glGenVertexArrays(1, &VAO);
+    glBindVertexArray(VAO);
+
+    // Creating the vertex buffer object
+    unsigned int VBO; // the id for the vertex buffer object
+    glGenBuffers(1, &VBO); // Generating id for VBO
+    glBindBuffer(GL_ARRAY_BUFFER, VBO); // Binds to GL_ARRAY_BUFFER on OpenGL context
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW); // Initializing vertex buffer
+
+    // Vertex shader
+    const char* vertexShaderCode = "#version 330 core\n"
+                                    "layout (location = 0) in vec3 aPos;\n"
+                                    "void main(){\n"
+                                    "gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+                                    "}\0";
+
+    // Creating a vertex shader object
+    unsigned int vertexShader;
+    vertexShader = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vertexShader, 1, &vertexShaderCode, NULL); // Assign shader code on vertex shader
+    glCompileShader(vertexShader);
+
+    // Checking if vertex shader is successfully compiled
+    int vertexShaderComplitationSuccess;
+    char vertexShaderInfoLog[512];
+    glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &vertexShaderComplitationSuccess);
+
+    if (!vertexShaderComplitationSuccess){
+        glGetShaderInfoLog(vertexShader, 512, NULL, vertexShaderInfoLog);
+        std::cout << "Error in vertex shader compilation, GLAD Error Message: " << vertexShaderInfoLog << std::endl;
+    }
+
+    // Fragment Shader
+    const char* fragmentShaderCode = "#version 330 core\n"
+                                        "out vec4 fragColour;\n"
+                                        "void main(){\n"
+                                        "fragColour = vec4(0.0f, 0.8f, 0.8f, 1.0f);\n"
+                                        "}\0";
+
+    // Initialize the fragment shader object
+    unsigned int fragmentShader;
+    fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fragmentShader, 1, &fragmentShaderCode, NULL);
+    glCompileShader(fragmentShader);
+
+    // Check if fragment shader compiled properly
+    int fragmentShaderCompilationSuccess;
+    char fragmentShaderInfoLog[512];
+    glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &fragmentShaderCompilationSuccess);
+
+    if (!fragmentShaderCompilationSuccess){
+        glGetShaderInfoLog(fragmentShader, 512, NULL, fragmentShaderInfoLog);
+        std::cout << "Error when compiling fragment shader, GLAD error message: " << fragmentShaderInfoLog << std::endl;
+    }
+
+    // Initializing the shader program object
+    unsigned int shaderProgram;
+    shaderProgram = glCreateProgram();
+    glAttachShader(shaderProgram, vertexShader);
+    glAttachShader(shaderProgram, fragmentShader);
+    glLinkProgram(shaderProgram);
+
+    // Check if shader program object linked shaders properly
+    int shaderProgramLinkingSuccess;
+    char shaderProgramInfoLog[512];
+    glGetProgramiv(shaderProgram, GL_LINK_STATUS, &shaderProgramLinkingSuccess);
+    
+    if (!shaderProgramLinkingSuccess){
+        glGetProgramInfoLog(shaderProgram, 512, NULL, shaderProgramInfoLog);
+        std::cout << "Error message when linking shaders, GLAD error message" << shaderProgramInfoLog << std::endl;
+    }
+
+    // Delete shader since we don't need them
+    glDeleteShader(fragmentShader);
+    glDeleteShader(vertexShader);
+
+    // Determines vertex attributes should be interpretted by creating the vertex buffer data format and enable attributes.
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0); 
+    glEnableVertexAttribArray(0);
+
     // Render Loop
     while(!glfwWindowShouldClose(window)){
         processInput(window); // Inputs
 
-    // Rendering commands
+        // Rendering commands
         glClearColor(0.0f, 0.4f, 0.4f, 1.0f); // Set color which clears frame
-        glClear(GL_COLOR_BUFFER_BIT); // Set which buffer bit were clearing (the colour one).
+        glClear(GL_COLOR_BUFFER_BIT); // Set which buffer bit were clearing (the colour one)
 
-        // Vertex data
-        float vertices[] = {
-            -0.5f, 0.5f, 0.0f,
-            0.5f, 0.5f, 0.0f,
-            0.0f, -0.5f, 0.0f
-        };
-
-        // Creating the vertex buffer object
-        unsigned int VBO; // the id for the vertex data
-        glGenBuffers(1, &VBO); // Generating id for VBO
-        glBindBuffer(GL_ARRAY_BUFFER, VBO); // Binds to GL_ARRAY_BUFFER on OpenGL context
-        glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW); // Put vertex data onto vertex buffer
-
-        // Vertex shader
-        const char* vertexShaderCode = "#version 330 core\n"
-                                        "layout (location = 0) in vec3 aPos;\n"
-                                        "void main(){\n"
-                                        "gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0)\n"
-                                        "}\0";
-
-        // Creating a vertex shader object
-        unsigned int vertexShader;
-        vertexShader = glCreateShader(GL_VERTEX_SHADER);
-        glShaderSource(vertexShader, 1, &vertexShaderCode, NULL); // Assign shader code on vertex shader
-        glCompileShader(vertexShader);
-
-        // Checking if vertex shader is successfully compiled
-        int success;
-        char infoLog[512];
-        glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
-
-        if (!success){
-            glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
-            std::cout << "Error in vertex shader compilation, GLAD Shader Error Message: " << infoLog << std::endl;
-        }
+        glUseProgram(shaderProgram); // Start of shader program
+        glBindVertexArray(VAO); // Bind VAO for this object
+        
+        glDrawArrays(GL_TRIANGLES, 0, 3);  // Draws shapes shaders, vertex attr. configs and vertex data
 
         glfwSwapBuffers(window); // Swaps front and back buffers
         glfwPollEvents(); // Checks if events are triggered
