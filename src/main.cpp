449253f8869d24efe678f46b464454e790dc4141
Sincere <sincerityegbu@gmail.com>
2022-02-20 10:27:18 +0000

    OpenGL Learning: Made a rectangle
    + Added ebos and indicies

--- src/main.cpp
+++ src/main.cpp
@@ -41,10 +41,22 @@ int main(){
 
     // Vertex data
     float vertices[] = {
-        0.0f, 0.5f, 0.0f,
-        -0.5f, -0.5f, 0.0f,
-        0.5f, -0.5f, 0.0f
+     0.5f,  0.5f, 0.0f,  // top right
+     0.5f, -0.5f, 0.0f,  // bottom right
+    -0.5f, -0.5f, 0.0f,  // bottom left
+    -0.5f,  0.5f, 0.0f   // top left 
     };
+    
+    unsigned int indices[] = {  // note that we start from 0!
+        0, 1, 3,   // first triangle
+        1, 2, 3    // second triangle
+    };  
+
+    // Creating the element buffer object 
+    unsigned int EBO; 
+    glGenBuffers(1, &EBO);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
 
     // Creating the vertex array object
     unsigned int VAO;
@@ -139,7 +151,10 @@ int main(){
         glUseProgram(shaderProgram); // Start of shader program
         glBindVertexArray(VAO); // Bind VAO for this object
         
-        glDrawArrays(GL_TRIANGLES, 0, 3);  // Draws shapes shaders, vertex attr. configs and vertex data
+        glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+
+        glBindVertexArray(0);
 
         glfwSwapBuffers(window); // Swaps front and back buffers
         glfwPollEvents(); // Checks if events are triggered
